ef4f235b5d9dc5948d6b1100c86498fd3217930c
joachim.kainz <joachim.kainz@c86e12c4-a0bc-11de-afa9-e300858c10cc>
2010-02-03 01:05:47 +0000

    initial version.
    git-svn-id: https://jolira-tools.googlecode.com/svn/wicket-stateless/trunk@1742 c86e12c4-a0bc-11de-afa9-e300858c10cc

---
+++ src/main/java/com/google/code/joliratools/StatelessAjaxFallbackLink.java
@@ -0,0 +1,101 @@
+package com.google.code.joliratools;
+
+/**
+ * 
+ */
+
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxEventBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.IAjaxCallDecorator;
+import org.apache.wicket.ajax.calldecorator.CancelEventIfNoAjaxDecorator;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
+import org.apache.wicket.ajax.markup.html.IAjaxLink;
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.IModel;
+
+/**
+ * Just like {@link AjaxFallbackLink}, but stateless.
+ * 
+ * @author jfk
+ * 
+ */
+public abstract class StatelessAjaxFallbackLink<T> extends Link<T> implements
+        IAjaxLink {
+    private static final long serialVersionUID = -133600842398684777L;
+
+    public StatelessAjaxFallbackLink(final String id) {
+        this(id, null);
+    }
+
+    public StatelessAjaxFallbackLink(final String id, final IModel<T> model) {
+        super(id, model);
+
+        add(new AjaxEventBehavior("onclick") {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected IAjaxCallDecorator getAjaxCallDecorator() {
+                return new CancelEventIfNoAjaxDecorator(
+                        StatelessAjaxFallbackLink.this.getAjaxCallDecorator());
+            }
+
+            @Override
+            public boolean getStatelessHint(final Component component) {
+                return true;
+            }
+
+            @Override
+            @SuppressWarnings("synthetic-access")
+            protected void onComponentTag(final ComponentTag tag) {
+                // only render handler if link is enabled
+                if (isLinkEnabled()) {
+                    super.onComponentTag(tag);
+                }
+            }
+
+            @Override
+            protected void onEvent(final AjaxRequestTarget target) {
+                onClick(target);
+            }
+        });
+    }
+
+    /**
+     * 
+     * @return call decorator to use or null if none
+     */
+    protected IAjaxCallDecorator getAjaxCallDecorator() {
+        return null;
+    }
+
+    /**
+     * Hints that this component is stateless.
+     * 
+     * @return always {@literal true}
+     * @see Link#getStatelessHint()
+     */
+    @Override
+    protected boolean getStatelessHint() {
+        return true;
+    }
+
+    /**
+     * @see Link#onClick()
+     */
+    @Override
+    public final void onClick() {
+        onClick(null);
+    }
+
+    /**
+     * Callback for the onClick event. If ajax failed and this event was
+     * generated via a normal link the target argument will be null
+     * 
+     * @param target
+     *            ajax target if this linked was invoked using ajax, null
+     *            otherwise
+     */
+    public abstract void onClick(final AjaxRequestTarget target);
+}
